ccd23d8ddcb411bae2f108a30951cbc33f57a1d2
bdnguye <bdnguye@student.matnat.uio.no>
2018-10-18 21:43:50 +0200

    Random testing.

--- test.cpp
+++ test.cpp
@@ -1,9 +1,21 @@
-//This is the test file for all testing purposes.
-#include <iostream>
-using namespace std;
-
-void main(){
-    int s = 4;
-    s /= 2;
-    cout << s << endl;
-}
+// expre_Using_delete.cpp  
+struct UDType   
+{  
+};  
+  
+int main()  
+{  
+   // Allocate a user-defined object, UDObject, and an object  
+   //  of type double on the free store using the  
+   //  new operator.  
+   UDType *UDObject = new UDType;  
+   double *dObject = new double;  
+   // Delete the two objects.  
+   delete UDObject;  
+   delete dObject;   
+   // Allocate an array of user-defined objects on the  
+   // free store using the new operator.  
+   UDType (*UDArr)[7] = new UDType[5][7];  
+   // Use the array syntax to delete the array of objects.  
+   delete [] UDArr;  
+}  
