296a9c273e169571bd9329738327448c53c5bafb
bdnguye <bdnguye@student.matnat.uio.no>
2018-10-18 21:43:40 +0200

    Added all test functions. Only error is pop not returning correct value.

--- linked_list.cpp
+++ linked_list.cpp
@@ -143,14 +143,19 @@ int main() {
     test.append(6);
     test.append(7);
     test.append(8);
-    test.append(9);
     test.append(10);
     test.print();
-    cout << "Pop first index: " << test.pop(0) << endl;
+    test.insert(9,8);
+    cout << "Inserted 9 in index 8:" << endl;
     test.print();
     test.remove(4);
     cout << "Removed index 4." << endl;
     test.print();
-    
+    cout << "Pop first index: " << test.pop(0) << endl;
+    test.print();
+    cout << "Pop 3 index: " << test.pop(3) << endl;
+    test.print();
+    cout << "Pop last index: " << test.pop() << endl;
+    test.print();
     return 0;
 }
