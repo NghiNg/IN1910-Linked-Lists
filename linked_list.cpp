1fe390258ff4d45c03c9f951306811f3914e99a6
bdnguye <bdnguye@student.matnat.uio.no>
2018-10-22 16:10:26 +0200

    Fixed remove and pop over size one to zero.

--- linked_list.cpp
+++ linked_list.cpp
@@ -114,6 +114,10 @@ class LinkedList {
             Node* before;
             Node* current;
             current = get_node(index);
+            if (size == 1) {
+                head = nullptr;
+                return;
+            }
             if (index == size-1) {
                 before = get_node(index-1);
                 before->next = nullptr;
@@ -143,26 +147,7 @@ class LinkedList {
 };
 
 int main() {
-    LinkedList test({1,2,3,4,5});
-    cout << "This is the initial list." << endl;
-    test.print();
-    cout << "Testing append, 4 integers." << endl;
-    test.append(6);
-    test.append(7);
-    test.append(8);
-    test.append(10);
-    test.print();
-    test.insert(9,8);
-    cout << "Inserted 9 in index 8:" << endl;
-    test.print();
-    test.remove(4);
-    cout << "Removed index 4." << endl;
-    test.print();
-    cout << "Pop first index: " << test.pop(0) << endl;
-    test.print();
-    cout << "Pop 3 index: " << test.pop(3) << endl;
-    test.print();
-    cout << "Pop last index: " << test.pop() << endl;
-    test.print();
-    return 0;
+    LinkedList test({11});
+    cout << test.pop(0) << endl;
+    cout << "test" << endl;
 }
