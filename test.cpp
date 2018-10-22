f802ca47d7142102d492a4a212b4a645d82e99e6
bdnguye <bdnguye@student.matnat.uio.no>
2018-10-22 23:34:08 +0200

    Testings.

--- test.cpp
+++ test.cpp
@@ -3,19 +3,26 @@ struct UDType
 {  
 };  
   
-int main()  
-{  
-   // Allocate a user-defined object, UDObject, and an object  
-   //  of type double on the free store using the  
-   //  new operator.  
-   UDType *UDObject = new UDType;  
-   double *dObject = new double;  
-   // Delete the two objects.  
-   delete UDObject;  
-   delete dObject;   
-   // Allocate an array of user-defined objects on the  
-   // free store using the new operator.  
-   UDType (*UDArr)[7] = new UDType[5][7];  
-   // Use the array syntax to delete the array of objects.  
-   delete [] UDArr;  
-}  
+int main() {
+    LinkedList test({1,2,3,4,5});
+    cout << "This is the initial list." << endl;
+    test.print();
+    cout << "Testing append, 4 integers." << endl;
+    test.append(6);
+    test.append(7);
+    test.append(8);
+    test.append(10);
+    test.print();
+    test.insert(9,8);
+    cout << "Inserted 9 in index 8:" << endl;
+    test.print();
+    test.remove(4);
+    cout << "Removed index 4." << endl;
+    test.print();
+    cout << "Pop first index: " << test.pop(0) << endl;
+    test.print();
+    cout << "Pop 3 index: " << test.pop(3) << endl;
+    test.print();
+    cout << "Pop last index: " << test.pop() << endl;
+    test.print();
+    return 0;
